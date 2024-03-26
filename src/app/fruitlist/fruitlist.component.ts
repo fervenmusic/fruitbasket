1f3f24307e1cc61a42f87039d39451c953f740c3
Ferven <120587289+fervenmusic@users.noreply.github.com>
2024-03-26 17:37:17 +0100

    first commit

---
+++ src/app/fruitlist/fruitlist.component.ts
@@ -0,0 +1,26 @@
+import { CommonModule } from '@angular/common';
+import { Component, inject } from '@angular/core';
+import { SinglefruitComponent } from './singlefruit/singlefruit.component';
+import { FruitlistdataService } from '../fruitlistdata.service';
+
+@Component({
+  selector: 'app-fruitlist',
+  standalone: true,
+  imports: [CommonModule, SinglefruitComponent],
+  templateUrl: './fruitlist.component.html',
+  styleUrl: './fruitlist.component.scss',
+})
+export class FruitlistComponent {
+  fruitlistdata = inject(FruitlistdataService);
+
+  fontColorGood = 'green';
+  fontColorBad = 'red';
+
+  addComment(comment: string, index: number) {
+    this.fruitlistdata.addCommentToFruit(comment, index); 
+  }
+
+  nameLog(name: string) {
+    console.log(name);
+  }
+}
